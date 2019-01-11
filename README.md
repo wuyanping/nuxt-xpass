54723e86dbd50611132e09b55b1794f7ada9cbc5
wuyanping <1063218918@qq.com>
2019-01-11 15:32:40 +0800

    init nuxt-xpass poject

---
+++ README.md
@@ -0,0 +1,34 @@
+# nuxt2 + element dashboard
+
+## Docs
+
+For detailed explanation on how things work, checkout the [Nuxt.js docs](https://github.com/nuxt/nuxt.js).
+
+## Build Setup
+
+```bash
+# install dependencies
+$ yarn
+
+# serve with hot reload at localhost:3000
+# using mock api to develop
+$ yarn mock
+
+# using mock api to develop which doesn't need login
+$ yarn mock:nologin
+
+# using backend api to develop
+$ yarn dev
+
+# build for production
+$ yarn build
+```
+
+## code style
+
+when you npm i or yarn, prettier has already installed
+
+the configuration file is .prettierrc
+
+Pre-commit Hook use [pretty-quick](https://github.com/azz/pretty-quick), maybe commit in terminal will be better, in IDE
+like webstorm it may get confused behavior😕
---
+++ src/plugins/axios.js
@@ -0,0 +1,44 @@
+import Vue from 'vue'
+import cookie from 'js-cookie'
+
+export default function(context) {
+  let {$axios, store, app, redirect} = context
+
+  $axios.onRequest(config => {
+    let url = config.url
+
+    url += url.indexOf('?') > -1 ? '&' : '?'
+    url += `token=${store.state.token}`
+
+    // 调用用户中心的接口需要加上projectNo
+    if (url.indexOf('/security/') > -1) {
+      url += `&projectNo=${process.env.PROJECT_NO}`
+    }
+
+    config.url = url
+
+    return config
+  })
+
+  $axios.onError(error => {
+    if (process.client) {
+      // axios 数据结构
+      let resp = error.response
+      let data = resp.data
+
+      Vue.$notify.error({
+        title: resp.status,
+        message: data.payload || data.msg
+      })
+
+      if (resp.status == 401) {
+        let path = app.router.options.base
+        cookie.remove('token', {path})
+        cookie.remove('userId', {path})
+        redirect('/login')
+      }
+    }
+    // TODO asyncData 的错误 需要日志监控
+    else console.log('error', error)
+  })
+}
